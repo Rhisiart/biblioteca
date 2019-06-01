d116e77d5efefe3cd894efd1bdb2912891f6163d
ricar <ricardobenfiquista13@gmail.com>
2019-06-01 16:57:55 +0100

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,32 @@ public class Main {
         Utilizador u = new Utilizador("Manuel","Ativo",new TipoUtilizador("aluno",10));
         Utilizador u1 = new Utilizador("Manue","Ativo",new TipoUtilizador("aluno",10));
         Utilizador u2 = new Utilizador("Manue","Ativo",new TipoUtilizador("aluno",10));
-        /*PropostaAquisicao propostaAquisicao = new PropostaAquisicao(LocalDate.now(),u,"Alice no Pais das Maravilhas");
+        PropostaAquisicao propostaAquisicao = new PropostaAquisicao(LocalDate.now(),u,"Alice no Pais das Maravilhas");
         Livro l = new Livro("Programar em Java", "FCA");
         Copia c = new Copia(1, l);
-        RequisicaoCompraCopia requisicaoCompraCopia = new RequisicaoCompraCopia("pendente", LocalDate.now(), "Oficio",l,u);
-        Encomenda encomenda = new Encomenda(LocalDate.now(),requisicaoCompraCopia);
-        EntradaNovoLivro entradaNovoLivro = new EntradaNovoLivro(LocalDate.now(),encomenda,c);
+        //RequisicaoCompraCopia requisicaoCompraCopia = new RequisicaoCompraCopia("pendente", LocalDate.now(), "Oficio",l,u);
+        //Encomenda encomenda = new Encomenda(LocalDate.now(),requisicaoCompraCopia);
+        //EntradaNovoLivro entradaNovoLivro = new EntradaNovoLivro(LocalDate.now(),encomenda,c);
         //... criação do objeto repositorio
-        Repositorio repo = new RepositorioMem();
-        repo.adicionaEntradaNovoLivro(entradaNovoLivro);
+        //Repositorio repo = new RepositorioMem();
+        //repo.adicionaEntradaNovoLivro(entradaNovoLivro);
         //...login Utilizador
-        Login login = new Login(u,"aaa");
-        Login login1 = new Login(u1,"a");
-        Login a = new Login(u2,"sadasda");
-        a.signIN();
-        login.signIN();
-        login1.signIN();
-        login.forgetPw("a");
-        System.out.println(login.getPassword());
-        System.out.println(login.getArrayRegisto());*/
+        //Login login = new Login(u,"aaa");
+        //Login login1 = new Login(u1,"a");
+        //Login a = new Login(u2,"sadasda");
+        //a.signIN();
+        //login.signIN();
+        //login1.signIN();
+        //login.forgetPw("a");
+        //System.out.println(login.getPassword());
+        //System.out.println(login.getArrayRegisto());
 
         //...Pagamento de coima
         Metodo_Pagamento dinheiro = new Metodo_Pagamento("dinheiro");
         Metodo_Pagamento paypal = new Metodo_Pagamento("paypal");
         Metodo_Pagamento mbway = new Metodo_Pagamento("mbway");
         Metodo_Pagamento multibanco = new Metodo_Pagamento("multibanco");
+        Coima coima = new Coima("qualquer",11.1,);
     }
 
 }
